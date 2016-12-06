b85ea1cc730b979a45b81d75466ecc249d3c8a5d
Kara Leary <learykara@gmail.com>
2016-12-06 14:28:58 -0500

    add some routes and stuff

--- models/user.js
+++ models/user.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
   name: {
-    roles: Array, // Possible to do array of strings?
+    type: String,
     unique: true,
   },
+  roles: [{ type: String }],
 })
 
 const User = mongoose.model('User', userSchema)
 
-module.exports = userSchema
+module.exports = User
--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,31 @@ const { Boat, User } = require('../models')
 const apiRouter = express.Router({ mergeParams: true })
 
 /* User API */
+apiRouter.get('/users', (req, res) => {
+  User.find((err, users) => {
+    if (err) {
+      return res.status(500).json({
+        message: 'Internal server error',
+      })
+    }
+    users.map(user => user.roles.map(role => console.log(role[0])))
+    // Set headers as application/json
+    res.json(users)
+  })
+})
+
 apiRouter.post('/users', (req, res) => {
   User.create({
     name: req.body.name,
-    roles: req.body.roles,
+    // Why is roles going in as a nested array?
+    roles: req.body.roles.split(','),
+  }, (err, user) => {
+    if (err) {
+      res.status(500).json({
+        message: 'Internal server error',
+      })
+    }
+    res.status(201).json(user)
   })
 })
 /* End User API */
@@ -27,16 +48,32 @@ apiRouter.get('/boats', (req, res) => {
 })
 
 apiRouter.post('/boats', (req, res) => {
-  Boat.create({
-    name: req.body.name,
-    sail: req.body.sail,
-  }, (err, boat) => {
+  const skipper = User.findOne({
+    _id: req.body.userId
+  }, (err, skipper) => {
     if (err) {
+      console.error(err)
       res.status(500).json({
         message: 'Internal server error',
       })
     }
-    res.status(201).json(boat)
+    if (!skipper) {
+      res.status(404).json({
+        message: 'Skipper not found',
+      })
+    }
+    Boat.create({
+      name: req.body.name,
+      sail: req.body.sail,
+      skipper: skipper,
+    }, (err, boat) => {
+      if (err) {
+        res.status(500).json({
+          message: 'Internal server error',
+        })
+      }
+      res.status(201).json(boat)
+    })
   })
 })
 /* End Boat API */
